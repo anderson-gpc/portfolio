4d9fcc7fd0295782bdd8136746988300abecde75
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-07-30 15:41:07 -0300

    chore: importando o componente na aplicação

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ import "./components/navbar_component.js";
 import "./components/header_component.js";
 import "./components/projects_component.js";
 import "./components/experience_component.js";
+import "./components/skills_component.js";
