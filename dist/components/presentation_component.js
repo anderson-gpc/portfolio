a20b3392653df2b88ba1e2002e7c7f2f9c9097d9
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-07-30 10:28:40 -0300

    chore: implementação do '.ts'

--- dist/components/presentation_component.js
+++ dist/components/presentation_component.js
@@ -6,7 +6,11 @@ class PresentationComponent extends HTMLElement {
     render() {
         const title = this.getAttribute('titulo') || '';
         this.innerHTML = `
-            <style>                
+            <style>
+                .container__cumprimentacao {
+                    padding: 1rem;
+                }
+                
                 .sublinhado {
                     width: 20%;
                     height: 1rem;
@@ -28,7 +32,7 @@ class PresentationComponent extends HTMLElement {
                 href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
                 rel="stylesheet"
             />
-            <section class="container py-1" aria-label="Cumprimentação">
+            <section class="container__cumprimentacao" aria-label="Cumprimentação">
 
                 <h2 class="display-1 d-block d-md-none">Olá!</h2>
                 <h2 class="display-4 d-none d-md-block">Olá!</h2>
