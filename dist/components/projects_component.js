cbd9320654a1ad71fa9e4aa03358e86fcbd8dd52
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-07-31 10:27:52 -0300

    chore: atualizando os projetos

--- dist/components/projects_component.js
+++ dist/components/projects_component.js
@@ -7,18 +7,18 @@ class ProjectsComponent extends HTMLElement {
     projects() {
         const containerProjects = document.querySelector("#container__projetos");
         const projetcts = [
-            { 'title': "Advice generator",
-                "src": "./images/advice.png",
-                "description": "Aplicação que gera conselhos para os usuários dinâmicamente",
-                "technologies": "TypeScript,SCSS",
-                "platform": "Web",
+            { 'title': "SigPonto",
+                "src": "./images/sigponto.jpg",
+                "description": "Aplicativo Android e Web responsivo para controle de ponto eletrônico de servidores, com autenticação via SIGAA e validação por geolocalização. O registro de ponto é permitido apenas dentro dos limites do campus. Fui Responsável pelo desenvolvimento do front-end com foco em responsividade e acessibilidade. Também foi criada uma versão web progressiva para uso direto no navegador, sem necessidade de instalação.",
+                "technologies": "Flutter,Django",
+                "platform": "Web, Android",
             },
             { 'title': "Advice generator",
                 "src": "./images/advice.png",
-                "description": "Aplicação que gera conselhos para os usuários dinâmicamente",
+                "description": "Aplicativo Web responsivo que exibe conselhos aleatórios consumindo a Advice Slip API. Desenvolvido com HTML, CSS, JavaScript e TypeScript, o app permite a geração de novos conselhos por meio da interação com um ícone de dado. Desenvolvi todo o projeto com foco em garantir a robustez da aplicação: caso a API externa esteja indisponível, o sistema utiliza um arquivo JSON local como fallback. Também assegurei a responsividade da interface em diferentes tamanhos de tela, proporcionando uma experiência consistente em diversos dispositivos.",
                 "technologies": "TypeScript,SCSS",
                 "platform": "Web",
-            }
+            },
         ];
         projetcts.forEach((project, index) => {
             const projectEl = document.createElement("card-component");
